6c08a5f949697312060299c3245f6a5dc13196f7
Infant Ajay Venus <infantajayv@duck.com>
2025-05-22 06:52:25 +0530

    ci: update base path in config and index html

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,13 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Use base URL for GitHub Pages deployment
+  // In development, this will be '/'
+  // In GitHub Actions, this will be set dynamically using the repository name
+  base: process.env.BASE_URL ? `/${process.env.BASE_URL}/` : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
